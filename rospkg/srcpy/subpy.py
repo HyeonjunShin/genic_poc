85a33f8b88adeb8e77c64baa34abf045ede44de3
HyeonjunShin <hyeonnnjnnn@gmail.com>
2025-12-02 15:25:58 +0900

    DEV: Modified the gitignore file and topic example codes.

---
+++ rospkg/srcpy/subpy.py
@@ -0,0 +1,25 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class MinimalSubscriber(Node):
+    def __init__(self):
+        super().__init__('python_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.listener_callback,
+            10
+        )
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'I heard: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MinimalSubscriber()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
